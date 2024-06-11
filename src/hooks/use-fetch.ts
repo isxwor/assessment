4bf76a35f173d11cb53828658674b9fcc250c538
isxwor <57859708+isxwor@users.noreply.github.com>
2024-06-11 14:46:41 +0530

    add fetch cache

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,5 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 
+const fetchCache = new Map();
+
 export const useFetch = <T>(url?: string) => {
   const [data, setData] = useState<T>();
   const [loading, setLoading] = useState(false);
@@ -13,9 +15,16 @@ export const useFetch = <T>(url?: string) => {
     setLoading(true);
     setError(null);
 
+    if (fetchCache.has(url)) {
+      setData(fetchCache.get(url));
+      setLoading(false);
+      return;
+    }
+
     try {
       const res = await fetch(url);
       const result = await res.json();
+      fetchCache.set(url, result);
       setData(result);
     } catch (e) {
       setError(e as Error);
